9d045467b5ab572ce0b22e5f684784fb873d0b85
QasimFareh <qaseemfareh123@gmail.com>
2026-04-26 08:07:07 +0300

    Update DoubleLinkedList.java

--- linked-list/double-linked-list/DoubleLinkedList.java
+++ linked-list/double-linked-list/DoubleLinkedList.java
@@ -1,6 +1,7 @@
 package lab6;
 
 
+
 public class DoubleLinkedList<T extends Comparable<T>> {
     Node<T> head;
 
@@ -33,29 +34,27 @@ public class DoubleLinkedList<T extends Comparable<T>> {
             while (curr.next != head && curr.data.compareTo(data) < 0) {
                 curr = curr.next;
             }
-            newData.next=curr;
-            newData.prev=curr.prev;
-            curr.prev=newData;
-            newData.prev.next=newData;
+            newData.next = curr;
+            newData.prev = curr.prev;
+            curr.prev = newData;
+            newData.prev.next = newData;
         }
-
-
     }
 
-    public void delete(T data){
+    public void delete(T data) {
         Node<T> curr = head.next;
 
-        if(head.next == head) {
-            return;
-        }
-
-        else if(head.next.data.compareTo(data) > 0) {
-            return;
-        }
-
-        else if(head.prev.data.compareTo(data) < 0){
-            return;
-        }
+// if(head.next == head) {
+// return;
+// }
+//
+// else if(head.next.data.compareTo(data) > 0) {
+// return;
+// }
+//
+// else if(head.prev.data.compareTo(data) < 0){
+// return;
+// }
 
         while (curr != head && curr.data.compareTo(data) < 0) {
             curr = curr.next;
@@ -66,7 +65,6 @@ public class DoubleLinkedList<T extends Comparable<T>> {
             curr.next.prev = curr.prev;
         }
     }
-
 
     public void traverse() {
         Node<T> curr = head.next;
@@ -85,5 +83,75 @@ public class DoubleLinkedList<T extends Comparable<T>> {
         System.out.println();
     }
 
+    public void traverseRev() {
+        Node<T> prev = head.prev;
+        if (prev == head) {
+            System.out.println("List is empty");
+            return;
+        }
+        while (prev != head) {
+            System.out.print(prev.data);
+            if (prev.prev != head) {
+                System.out.print(" <-> ");
+            }
+            prev = prev.prev;
+        }
+        System.out.print("<-> head");
+        System.out.println();
+    }
+
+    public void removeDublicate() {
+        Node<T> curr = head.next;
+        if (curr == head)
+            return;
+        Node<T> temp = new Node<>(curr.data);
+        while (curr != head) {
+            if (curr.data.compareTo(temp.data) == 0) {
+                temp.next = curr.next;
+                curr.next.prev=temp;
+            }
+            else
+                temp=curr;
+
+            curr = curr.next;
+        }
+    }
+
+    public boolean isPalendrom(){
+        Node<T> first = head.next;
+        Node<T> last = head.prev;
+        boolean zozo = false;
+
+        while(first!=last&&first.next!=last){
+            if(first.data.compareTo(last.data)!=0)
+                return false;
+        }
+        first=first.next;
+        last=last.prev;
+
+        return true;
+    }
+
+    public void rotateBy(int k) {
+        if (head.next == head || k == 0) return;
+
+        Node<T> curr = head.next;
+
+        for (int i = 0; i < k; i++) {
+            curr = curr.next;
+        }
+
+        Node<T> newHead = curr;
+        Node<T> oldFirst = head.next;
+        Node<T> oldLast = head.prev;
+        Node<T> beforeNewHead = newHead.prev;
+
+        oldLast.next = oldFirst;
+        oldFirst.prev = oldLast;
+        beforeNewHead.next = head;
+        head.prev = beforeNewHead;
+        head.next = newHead;
+        newHead.prev = head;
+    }
 
 }
