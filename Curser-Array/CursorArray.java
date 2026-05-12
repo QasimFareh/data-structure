1750ae173c64cd18e7778a631f818affc3b175b4
QasimFareh <qaseemfareh123@gmail.com>
2026-05-12 18:34:55 +0300

    Update CursorArray.java

--- Curser-Array/CursorArray.java
+++ Curser-Array/CursorArray.java
@@ -1,4 +1,4 @@
-public class CursorArray<T extends Comparable<CursorArray>> {
+public class CursorArray<T extends Comparable<T>> {
     CNode[] ca;
 
     CursorArray(int capacity) {
@@ -104,4 +104,98 @@ public class CursorArray<T extends Comparable<CursorArray>> {
         ca[p] = new CNode(data, curr);
         ca[prev].next = p;
     }
+
+    void merge(int l1, int l2) {
+        int p1 = l1;
+        int c1 = ca[l1].next;
+        int c2 = ca[l2].next;
+        while (c1 != 0 && c2 != 0) {
+            if (ca[c2].data.compareTo(ca[c1].data) < 0) {
+                int nextC2 = ca[c2].next;
+                ca[p1].next = c2;
+                ca[c2].next = c1;
+                p1 = c2;
+                c2 = nextC2;
+            } else {
+                p1 = c1;
+                c1 = ca[c1].next;
+            }
+        }
+        if (c2 != 0) {
+            ca[p1].next = c2;
+        }
+        ca[l2].next = 0;
+    }
+
+    boolean isSubset(int l1, int l2) {
+        int p1 = ca[l1].next;
+        while (p1 != 0) {
+            boolean found = false;
+            int p2 = ca[l2].next;
+            while (p2 != 0) {
+                if (ca[p1].data.compareTo(ca[p2].data) == 0) {
+                    found = true;
+                    break;
+                }
+                p2 = ca[p2].next;
+            }
+            if (!found) {
+                return false;
+            }
+            p1 = ca[p1].next;
+        }
+        return true;
+    }
+
+    void reverse(int l) {
+        int prev = 0;
+        int curr = ca[l].next;
+        while (curr != 0) {
+            int next = ca[curr].next;
+            ca[curr].next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ca[l].next = prev;
+    }
+
+    void swap(int l, T x, T y) {
+
+        if (x.compareTo(y) == 0)
+            return;
+
+        int prevX = l;
+        int currX = ca[l].next;
+
+        while (currX != 0 &&
+                ca[currX].data.compareTo(x) != 0) {
+
+            prevX = currX;
+            currX = ca[currX].next;
+        }
+
+        int prevY = l;
+        int currY = ca[l].next;
+
+        while (currY != 0 &&
+                ca[currY].data.compareTo(y) != 0) {
+
+            prevY = currY;
+            currY = ca[currY].next;
+        }
+
+        if (currX == 0 || currY == 0)
+            return;
+
+        ca[prevX].next = currY;
+        ca[prevY].next = currX;
+
+        int temp = ca[currX].next;
+        ca[currX].next = ca[currY].next;
+        ca[currY].next = temp;
+    }
+
+
 }
+
+
